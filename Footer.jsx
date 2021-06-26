c0b1a2f56fa8c1971834245542a7a5985e6d985f
DEEPAK CHOUDHARY <67619792+deepakchoudhary2030@users.noreply.github.com>
2021-06-26 17:21:25 +0530

    Add files via upload

---
+++ Footer.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "../styles.css";
+function Footer() {
+  return (
+    <div className="footer">
+      <p>Copyright by ShapeAI {new Date().getFullYear()}</p>
+    </div>
+  );
+}
+export default Footer;
---
+++ Info.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "../styles.css";
+function Info() {
+  return (
+    <div className="note">
+      <h1>java script Bootcamp</h1>
+      <p>here is the 7 days js bootcamp</p>
+    </div>
+  );
+}
+export default Info;
